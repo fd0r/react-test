9cc5230103eab23316088bd8294833d412784b29
Luis Montero <luismontero@hotmail.fr>
2018-11-12 00:37:01 +0100

    Updates

---
+++ precache-manifest.165422fb294f049fd9e8521d2c9fd5ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "745cc6a50a47403619a0",
+    "url": "/react-test/static/js/runtime~main.745cc6a5.js"
+  },
+  {
+    "revision": "cf2580acd1073cabfdb0",
+    "url": "/react-test/static/js/main.cf2580ac.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-test/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "cf2580acd1073cabfdb0",
+    "url": "/react-test/static/css/main.b56b8d3c.chunk.css"
+  },
+  {
+    "revision": "5c92c5016b39017942770cd6a9b15dc9",
+    "url": "/react-test/index.html"
+  }
+];
